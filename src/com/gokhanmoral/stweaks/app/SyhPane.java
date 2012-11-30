d5f599e6fce3057e2f2b51e406b3f80b9ec91a40
Gokhan Moral <gm@alumni.bilkent.edu.tr>
2012-11-30 13:27:05 +0200

    move some hardcoded layout info to xmls

--- src/com/gokhanmoral/stweaks/app/SyhPane.java
+++ src/com/gokhanmoral/stweaks/app/SyhPane.java
@@ -5,8 +5,7 @@ import java.util.List;
 
 import android.app.Activity;
 import android.content.Context;
-import android.graphics.Color;
-import android.graphics.Typeface;
+import android.view.LayoutInflater;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -20,22 +19,15 @@ public class SyhPane {
 	{
 		Context context = activity;
 		
-        //TODO: Move this to xml
 		TextView paneNameView = new TextView(context);
-		paneNameView.setBackgroundColor(Color.DKGRAY);
-		paneNameView.setTextColor(Color.LTGRAY);
-        paneNameView.setText(this.name.toUpperCase());
-        paneNameView.setTypeface(null, Typeface.BOLD);
-        layout.addView(paneNameView); 
+		paneNameView = (TextView)LayoutInflater.from(context).inflate(R.layout.template_panelname, layout, false);
+		paneNameView.setText(this.name.toUpperCase());
+		layout.addView(paneNameView); 
         
-        //TODO: Move this to xml
 		if ((this.description != null) && (!this.description.equals("")))
 		{
 	        TextView paneDescriptionView = new TextView(context);
-	        paneDescriptionView.setBackgroundColor(Color.BLACK);
-	        paneDescriptionView.setTextColor(Color.LTGRAY);
-	        //paneDescriptionView.setTextSize(paneNameView.getTextSize()*0.6f);
-	        paneDescriptionView.setTypeface(Typeface.defaultFromStyle(Typeface.ITALIC), Typeface.ITALIC);
+	        paneDescriptionView = (TextView)LayoutInflater.from(context).inflate(R.layout.template_paneldesc, layout, false);
 	        paneDescriptionView.setText(this.description);
 	        paneDescriptionView.setPadding(0, 5, 0, 10);
 	        layout.addView(paneDescriptionView);  
